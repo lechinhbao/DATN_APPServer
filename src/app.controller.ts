3415f07883b29331207207afac1421f474dec4ce
lechinhbao <lechinhbao3477@gmail.com>
2024-06-03 22:48:50 +0700

    bao

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,7 @@ export class AppController {
   }
   constructor(private readonly appService: AppService) { }
 
-  
+
   @Get('')
   async table(@Res() res: Response) {
     return res.redirect('/auth/loginWeb')
